02e4420184d31edc2c445b03ef46514b0a4762ed
John Ferguson <JAFerguson952@gmail.com>
2024-09-26 23:27:41 +0100

    added adaptive submodule, enabled unit test logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,25 @@
 
 pub mod gauss;
+pub mod adaptive;
+
+
+//-------------------------------------------------------------------------------------------------
+//{{{ mod: tests
+#[cfg(test)]
+mod tests
+{
+    use ctor::ctor;
+    use topohedral_tracing::*;
+
+    #[ctor]
+    fn init_logger() {
+        init().unwrap();
+    }
+
+    #[test]
+    fn test_logging() 
+    {
+        info!("Logging is working!");
+    }
+}
+//}}}
