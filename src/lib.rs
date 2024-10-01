a977e00f052128f6ba8fbe58fe1b7c2ed3820d81
John Ferguson <JAFerguson952@gmail.com>
2024-10-01 23:11:45 +0100

    added new modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,28 @@
+//! This crate provides a collection of quadature rules for numerical integration along with 
+//! algorithms for adaptive integration build on top of these rules.
+//!
+//! Longer description of module
+//--------------------------------------------------------------------------------------------------
 
+//{{{ crate imports 
+//}}}
+//{{{ std imports 
+//}}}
+//{{{ dep imports 
+//}}}
+//--------------------------------------------------------------------------------------------------
+
+
+
+
+mod common;
+
+pub use common::{OptionsError, OptionsStruct};
 pub mod gauss;
 pub mod adaptive;
+pub mod fixed;
+
+
 
 
 //-------------------------------------------------------------------------------------------------
@@ -10,6 +32,7 @@ mod tests
 {
     use ctor::ctor;
     use topohedral_tracing::*;
+    use super::*;
 
     #[ctor]
     fn init_logger() {
@@ -21,5 +44,7 @@ mod tests
     {
         info!("Logging is working!");
     }
+
+
 }
 //}}}
