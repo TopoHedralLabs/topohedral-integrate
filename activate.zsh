59347c8d89ab8566eaf2899cf6e9e17b39527d30
John Ferguson <JAFerguson952@gmail.com>
2024-10-01 23:09:02 +0100

    changed level of trace

--- activate.zsh
+++ activate.zsh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 export RUSTDOCFLAGS="--html-in-header $(pwd)/docs/html/custom-header.html --document-private-items"
 export CARGO_FEATURE_ENABLE_TRACE=1
-export TOPO_LOG=all=info
+export TOPO_LOG=all=debug
