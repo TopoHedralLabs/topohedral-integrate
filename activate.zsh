e0ac1d05cb7896ce2cdf4c6d9513f43377b921a5
John Ferguson <JAFerguson952@gmail.com>
2025-04-27 19:14:26 +0100

    migrated to cloudsmith

--- activate.zsh
+++ activate.zsh
@@ -1,7 +1,1 @@
 #!/bin/zsh
-
-
-export CARGO_REGISTRIES_CLOUDSMITH_TOKEN=$(op read "op://dev/cloudsmith-api-key1/credential")
-
-token=$(op read "op://dev/cloudsmith-default-token/credential")
-export CARGO_REGISTRIES_CLOUDSMITH_INDEX="https://dl.cloudsmith.io/$token/topohedrallabs/topohedral/cargo/index.git"
